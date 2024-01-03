335e95702ce9556d8a68af643a097e1fad2105b2
MVBPinho <marcos.vb.pinho@live.com>
2024-01-02 23:58:05 -0300

    Implementing Content Negotiation with JSON, XML and YAML

---
+++ 13_ContentNegotiation/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(6, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(7, 'Kentucky - US', 'Muhammad', 'Male', 'Alai');
