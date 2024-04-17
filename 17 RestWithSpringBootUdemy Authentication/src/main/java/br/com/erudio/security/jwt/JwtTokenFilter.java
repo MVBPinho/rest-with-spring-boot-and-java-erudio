8611ac84df676a57bc201d05fe2afc63b1bdab29
MVBPinho <marcos.vb.pinho@live.com>
2024-04-17 00:14:54 -0300

    Implementing Authenticatin With JWT

---
+++ 17 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,64 @@
+package br.com.erudio.security.jwt;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import br.com.erudio.exceptions.ExceptionResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtTokenFilter extends GenericFilterBean{
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
+	public JwtTokenFilter(JwtTokenProvider tokenProvider) {
+		this.tokenProvider = tokenProvider;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try {
+		String token = tokenProvider.resolveToken((HttpServletRequest) request);
+		if (token != null && tokenProvider.validateToken(token)) {
+			Authentication auth = tokenProvider.getAuthentication(token);
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		chain.doFilter(request, response);
+		} catch (JWTDecodeException e) {
+			writeExceptionToResponse(response, request, e, HttpStatus.BAD_REQUEST);
+		} catch (TokenExpiredException e) {
+			writeExceptionToResponse(response, request, e, HttpStatus.UNAUTHORIZED);
+		} catch (Exception e) {
+			writeExceptionToResponse(response, request, e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	private void writeExceptionToResponse(ServletResponse response, ServletRequest request, Exception e, HttpStatus httpStatus) throws IOException {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), ((HttpServletRequest) request).getRequestURI());
+		HttpServletResponse myResponse = (HttpServletResponse) response;
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String jsonResponse = ow.writeValueAsString(exceptionResponse);
+		myResponse.setStatus(httpStatus.value());
+		myResponse.setContentType("application/json");
+		myResponse.getWriter().write(jsonResponse);
+	}
+}
---
+++ 17 RestWithSpringBootUdemy Authentication/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
+	('marcos', 'Marcos Pinho', '17c81fb907326cd224889f0fbdc1aa7b8b6a347b65a4bb26b21cf0e5fea4f9dfd0d857b153157cc2', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
