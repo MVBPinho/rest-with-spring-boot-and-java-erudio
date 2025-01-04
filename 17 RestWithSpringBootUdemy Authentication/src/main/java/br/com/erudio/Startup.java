fb2b142c30b006b84dcd83e1d941de6d9f45a384
MVBPinho <marcos.vb.pinho@live.com>
2025-01-03 23:16:57 -0300

    Add collections, ajuste na senha bd, e criacao dos usuarios para oauth

--- 17 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/Startup.java
+++ 17 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/Startup.java
@@ -16,17 +16,19 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 		
-		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
-		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
-		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-		passwordEncoder.setDefaultPasswordEncoderForMatches(passwordEncoder);
-		
-		String result1 = passwordEncoder.encode("admin123");
-		String result2 = passwordEncoder.encode("admin234");
-		
-		System.out.println("My hash result1 " + result1);
-		System.out.println("My hash result2 " + result2);
+//		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+//
+//		Map<String, PasswordEncoder> encoders = new HashMap<>();
+//		encoders.put("pbkdf2", pbkdf2PasswordEncoder);
+//		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+//		passwordEncoder.setDefaultPasswordEncoderForMatches(passwordEncoder);
+//
+//		String result1 = passwordEncoder.encode("teste123");
+//		String result2 = passwordEncoder.encode("jpa222");
+//		String result3 = passwordEncoder.encode("senha123");
+//
+//		System.out.println("My hash result1 " + result1);
+//		System.out.println("My hash result2 " + result2);
+//		System.out.println("My hash result3 " + result2);
 	}
 }
--- 17 RestWithSpringBootUdemy Authentication/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ 17 RestWithSpringBootUdemy Authentication/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,4 +1,4 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-	('marcos', 'Marcos Pinho', '17c81fb907326cd224889f0fbdc1aa7b8b6a347b65a4bb26b21cf0e5fea4f9dfd0d857b153157cc2', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro Costa', '98097dc7303ff2f737b7dc40507505565fffbf58ba8623dcf1b9675c9e6e87fdfbe29194cc602ace', b'1', b'1', b'1', b'1'),
+	('teste', 'teste teste', '394cfaf4f3181f5f7eb2cfb904e67a43fbc09c867e16c0bda960ce1f49d6bcacf020aeddbd7fb37f', b'1', b'1', b'1', b'1'),
+	('marcos', 'Marcos Pinho', '394cfaf4f3181f5f7eb2cfb904e67a43fbc09c867e16c0bda960ce1f49d6bcacf020aeddbd7fb37f', b'1', b'1', b'1', b'1');
