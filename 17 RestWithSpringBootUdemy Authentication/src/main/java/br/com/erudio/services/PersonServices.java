5bac53c318ca1f9b3099a091a568e6d63f09a91b
MVBPinho <marcos.vb.pinho@live.com>
2025-01-09 20:27:24 -0300

    Realizado paginacao no findAll

--- 17 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/services/PersonServices.java
+++ 17 RestWithSpringBootUdemy Authentication/src/main/java/br/com/erudio/services/PersonServices.java
@@ -8,6 +8,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.erudio.controllers.PersonController;
@@ -26,13 +28,15 @@ public class PersonServices {
 	@Autowired
 	PersonRepository repository;
 
-	public List<PersonVO> findAll() {
+	public Page<PersonVO> findAll(Pageable pageable) {
 		logger.info("Finding all people!");
 
-		var persons = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
-		persons.stream()
-				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
-		return persons;
+		var personPage = repository.findAll(pageable);
+
+		var personVosPages = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+		personVosPages.map(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+
+		return personVosPages;
 	}
 
 	public PersonVO findById(Long id) {
