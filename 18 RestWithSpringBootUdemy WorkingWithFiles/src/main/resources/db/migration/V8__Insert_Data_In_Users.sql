aad734cda3f010f0167ad43de32e5ca594d9cfa4
MVBPinho <marcos.vb.pinho@live.com>
2025-01-12 21:27:39 -0300

    Add projeto para uploads de arquivos, add service, exceptions, config e VO

---
+++ 18 RestWithSpringBootUdemy WorkingWithFiles/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '98097dc7303ff2f737b7dc40507505565fffbf58ba8623dcf1b9675c9e6e87fdfbe29194cc602ace', b'1', b'1', b'1', b'1'),
+	('teste', 'teste teste', '394cfaf4f3181f5f7eb2cfb904e67a43fbc09c867e16c0bda960ce1f49d6bcacf020aeddbd7fb37f', b'1', b'1', b'1', b'1'),
+	('marcos', 'Marcos Pinho', '394cfaf4f3181f5f7eb2cfb904e67a43fbc09c867e16c0bda960ce1f49d6bcacf020aeddbd7fb37f', b'1', b'1', b'1', b'1');
---
+++ 18 RestWithSpringBootUdemy WorkingWithFiles/src/test/java/br/com/erudio/integrationtests/controller/withjson/BookControllerJsonTest.java
@@ -0,0 +1,267 @@
+package br.com.erudio.integrationtests.controller.withjson;
+
+import br.com.erudio.configs.TestConfigs;
+import br.com.erudio.data.vo.v1.security.AccountCredentialsVO;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.erudio.integrationtests.vo.BookVO;
+import br.com.erudio.integrationtests.vo.TokenVO;
+import br.com.erudio.integrationtests.vo.wrappers.WrapperBookVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,properties = {"server.port=8888"})
+@TestMethodOrder(OrderAnnotation.class)
+public class BookControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static BookVO book;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        book = new BookVO();
+    }
+
+    @Test
+    @Order(1)
+    public void authorization() {
+        AccountCredentialsVO user = new AccountCredentialsVO();
+        user.setUsername("leandro");
+        user.setPassword("teste123");
+
+        var token =
+                given()
+                        .basePath("/auth/signin")
+                        .port(TestConfigs.SERVER_PORT)
+                        .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                        .body(user)
+                        .when()
+                        .post()
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(TokenVO.class)
+                        .getAccessToken();
+
+        specification =
+                new RequestSpecBuilder()
+                        .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + token)
+                        .setBasePath("/api/book/v1")
+                        .setPort(TestConfigs.SERVER_PORT)
+                        .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                        .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                        .build();
+    }
+
+    @Test
+    @Order(2)
+    public void testCreate() throws JsonMappingException, JsonProcessingException {
+
+        mockBook();
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(book)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        book = objectMapper.readValue(content, BookVO.class);
+
+        assertNotNull(book.getId());
+        assertNotNull(book.getTitle());
+        assertNotNull(book.getAuthor());
+        assertNotNull(book.getPrice());
+        assertTrue(book.getId() > 0);
+        assertEquals("Docker Deep Dive", book.getTitle());
+        assertEquals("Nigel Poulton", book.getAuthor());
+        assertEquals(55.99, book.getPrice());
+    }
+
+    @Test
+    @Order(3)
+    public void testUpdate() throws JsonMappingException, JsonProcessingException {
+
+        book.setTitle("Docker Deep Dive - Updated");
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(book)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        BookVO bookUpdated = objectMapper.readValue(content, BookVO.class);
+
+        assertNotNull(bookUpdated.getId());
+        assertNotNull(bookUpdated.getTitle());
+        assertNotNull(bookUpdated.getAuthor());
+        assertNotNull(bookUpdated.getPrice());
+        assertEquals(bookUpdated.getId(), book.getId());
+        assertEquals("Docker Deep Dive - Updated", bookUpdated.getTitle());
+        assertEquals("Nigel Poulton", bookUpdated.getAuthor());
+        assertEquals(55.99, bookUpdated.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void testFindById() throws JsonMappingException, JsonProcessingException {
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .pathParam("id", book.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        BookVO foundBook = objectMapper.readValue(content, BookVO.class);
+
+        assertNotNull(foundBook.getId());
+        assertNotNull(foundBook.getTitle());
+        assertNotNull(foundBook.getAuthor());
+        assertNotNull(foundBook.getPrice());
+        assertEquals(foundBook.getId(), book.getId());
+        assertEquals("Docker Deep Dive - Updated", foundBook.getTitle());
+        assertEquals("Nigel Poulton", foundBook.getAuthor());
+        assertEquals(55.99, foundBook.getPrice());
+    }
+
+    @Test
+    @Order(5)
+    public void testDelete() {
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .pathParam("id", book.getId())
+                .when()
+                .delete("{id}")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    @Order(6)
+    public void testFindAll() throws JsonMappingException, JsonProcessingException {
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .queryParams("page", 0 , "limit", 10, "direction", "asc")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        WrapperBookVO wrapper = objectMapper.readValue(content, WrapperBookVO.class);
+        var books = wrapper.getEmbedded().getBooks();
+
+        BookVO foundBookOne = books.get(0);
+
+        assertNotNull(foundBookOne.getId());
+        assertNotNull(foundBookOne.getTitle());
+        assertNotNull(foundBookOne.getAuthor());
+        assertNotNull(foundBookOne.getPrice());
+        assertTrue(foundBookOne.getId() > 0);
+        assertEquals("Big Data: como extrair volume, variedade, velocidade e valor da avalanche de informação cotidiana", foundBookOne.getTitle());
+        assertEquals("Viktor Mayer-Schonberger e Kenneth Kukier", foundBookOne.getAuthor());
+        assertEquals(54.0, foundBookOne.getPrice());
+
+        BookVO foundBookFive = books.get(4);
+
+        assertNotNull(foundBookFive.getId());
+        assertNotNull(foundBookFive.getTitle());
+        assertNotNull(foundBookFive.getAuthor());
+        assertNotNull(foundBookFive.getPrice());
+        assertTrue(foundBookFive.getId() > 0);
+        assertEquals("Domain Driven Design", foundBookFive.getTitle());
+        assertEquals("Eric Evans", foundBookFive.getAuthor());
+        assertEquals(92.0, foundBookFive.getPrice());
+    }
+
+    @Test
+    @Order(9)
+    public void testHATEOAS() throws JsonMappingException, JsonProcessingException {
+
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .queryParams("page", 0 , "limit", 10, "direction", "asc")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        String baseUrl = "http://localhost:8888/api/book/v1";
+
+        String[] bookLinks = {
+                "/12",
+                "/3",
+                "/5"
+        };
+
+        for (String bookLink : bookLinks) {
+            assertTrue(content.contains("\"_links\":{\"self\":{\"href\":\"" + baseUrl + bookLink + "\"}}}"),
+                    "HATEOAS link for book " + bookLink + " is missing or incorrect.");
+        }
+
+        Map<String, String> paginationLinks = Map.of(
+                "first", "?direction=asc&page=0&size=12&sort=title,asc",
+                "self", "?page=0&size=12&direction=asc",
+                "next", "?direction=asc&page=1&size=12&sort=title,asc",
+                "last", "?direction=asc&page=1&size=12&sort=title,asc"
+        );
+
+        for (Map.Entry<String, String> link : paginationLinks.entrySet()) {
+            assertTrue(content.contains("\"" + link.getKey() + "\":{\"href\":\"" + baseUrl + link.getValue() + "\"}"),
+                    "HATEOAS link for " + link.getKey() + " is missing or incorrect.");
+        }
+
+        String pageInfo = "\"page\":{\"size\":12,\"totalElements\":15,\"totalPages\":2,\"number\":0}}";
+        assertTrue(content.contains(pageInfo), "Page information is missing or incorrect.");
+    }
+
+    private void mockBook() {
+        book.setTitle("Docker Deep Dive");
+        book.setAuthor("Nigel Poulton");
+        book.setPrice(Double.valueOf(55.99));
+        book.setLaunchDate(new Date());
+    }
+}
